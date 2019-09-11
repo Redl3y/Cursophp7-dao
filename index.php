f8e7ab80aa36eaa54835853eb8a4a65d9c925c12
redley21 <redley_redward@hotmail.com>
2019-09-11 13:44:25 -0400

    metodos de lista

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <?php     
 
 require_once("config.php");
+//Carrega um usuario
+//$jose = new Usuario();
+//$jose->loadbyId(2);
+//echo $jose;
 
-$jose = new Usuario();
-$jose->loadbyId(2);
-echo $jose;
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//carrega um usuário usando o login e a senha
+
+$usuario = new Usuario();
+$usuario->login("red","1234567");
+
+echo $usuario;
 ?>
